cf6da111f3501290bf65abf0aedd6a058e19f3f0
jilani <shuja.jilani@gmail.com>
2026-05-14 12:39:42 +0530

    feat: setup backend foundation, project structure and initial flyway configuration

---
+++ vitamania-backend/src/main/java/com/vitamania/VitamaniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vitamania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitamaniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitamaniaBackendApplication.class, args);
+	}
+
+}
---
+++ vitamania-backend/src/test/java/com/vitamania/VitamaniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitamania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitamaniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
